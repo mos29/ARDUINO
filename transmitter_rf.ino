ba55aecab594c9cd6d81505c276ef4310edaea94
mos <sharmilaborah@gmail.com>
2024-06-12 11:21:08 +0530

    Update transmitter_rf.ino

--- transmitter_rf.ino
+++ transmitter_rf.ino
@@ -1,31 +1,4 @@
-/*
-  Debounce
 
-  Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
-  press), the output pin is toggled from LOW to HIGH or HIGH to LOW. There's a
-  minimum delay between toggles to debounce the circuit (i.e. to ignore noise).
-
-  The circuit:
-  - LED attached from pin 13 to ground through 220 ohm resistor
-  - pushbutton attached from pin 2 to +5V
-  - 10 kilohm resistor attached from pin 2 to ground
-
-  - Note: On most Arduino boards, there is already an LED on the board connected
-    to pin 13, so you don't need any extra components for this example.
-
-  created 21 Nov 2006
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Limor Fried
-  modified 28 Dec 2012
-  by Mike Walters
-  modified 30 Aug 2016
-  by Arturo Guadalupi
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Debounce
-*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
